a08724037762c5eec512afb89dc64d162e20aa0a
seijirom <seijiro.moriyama@gmail.com>
2016-01-18 00:28:40 +0900

    changed slow_analog_output.rb to require rp_socket

--- slow_analog_input2.rb
+++ slow_analog_input2.rb
@@ -1,27 +1,7 @@
 require 'socket'
+require './rp_socket'
 
-class RP
-  def initialize
-    @socket = TCPSocket.open '192.168.0.5', 5000
-  end
-
-  def send command
-    @socket.puts command + "\r" 
-    @socket.flush
-  end
-
-  def query command
-    @socket.puts command + "\r"
-    @socket.flush
-    @socket.gets
-  end
-
-  def close
-    @socket.close
-  end
-end
-
-rp = RP.new
+rp = RP.new '192.168.0.5'
 
 rp.send "ANALOG:PIN AOUT3, 1.60"
 volts = rp.query "ANALOG:PIN? AIN3"
